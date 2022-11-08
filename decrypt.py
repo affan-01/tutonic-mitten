92b46be9d538aa591a3460b71f7831d702afd330
Mohammed Affan <affan.noor35@gmail.com>
2022-11-08 16:35:38 +0530

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,14 +1,20 @@
+#importing the library
 from cryptography.fernet import Fernet
 
+#opening the file which has the key
 with open('filekey','rb') as filekey:
     key = filekey.read()
 
+#using the key
 f = Fernet(key)
 
+#reading the ciphertext
 with open('ciphertext','rb') as ciphertext:
     text = ciphertext.read()
 
+#decrypting the ciphertext
 decrypted_text = f.decrypt(text)
 
+#writing the decrypted text back into a file
 with open('original_text','wb') as original_text:
     original_text.write(decrypted_text)
